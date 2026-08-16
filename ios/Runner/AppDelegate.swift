ec2a39e532a45dfd51781b4bc6cca2436d9a1ed2
Mayank27092003 <148036623+Mayank27092003@users.noreply.github.com>
2026-08-16 17:50:03 +0530

    fix(ios): wrap GoogleMaps in canImport, add bridging header include, and enable verbose build logging

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,8 @@
 import Flutter
 import UIKit
+#if canImport(GoogleMaps)
 import GoogleMaps
+#endif
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,7 +10,9 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    #if canImport(GoogleMaps)
     GMSServices.provideAPIKey("AIzaSyDcAw5j9JR1kWYLosJMwi8dqMPLF0x3OBc")
+    #endif
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,1 +1,4 @@
 #import "GeneratedPluginRegistrant.h"
+#if __has_include(<GoogleMaps/GoogleMaps.h>)
+#import <GoogleMaps/GoogleMaps.h>
+#endif
